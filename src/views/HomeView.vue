dd35a69a559ff36fdef82e5e996793481f5572cc
Tamás Szűcs <x-tamas.szucs2@wizzair.com>
2023-05-18 14:16:03 +0200

    Creating MessageDisplay component along with MessageDisplay.spec.js unit test file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,19 @@
       <div class="home">
         <h1>This is the home of practicing testing.</h1>
         <p>Enjoy!</p>
+        <MessageDisplay />
       </div>
   </section>
 </template>
 
 <script>
 // @ is an alias to /src
+import MessageDisplay from '@/components/MessageDisplay.vue';
 
 export default {
+  components: {
+    MessageDisplay,
+  }
 }
 </script>
 
